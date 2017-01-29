d25f9a660f5b350cabf25f352bd24c4a878e7f39
Alexandre Rivaux <alex@bonjour-lab.com>
2017-01-29 17:23:35 +0100

    20170129 mon premier skecth

--- cours/20170129_monPremierDessin/MyFirstSkecth.js
+++ cours/20170129_monPremierDessin/MyFirstSkecth.js
@@ -1,27 +1,27 @@
-var diameter;
-var ampY;
+var diameter; //diameter of the ellipse
+var ampY; //max amplitude
 
 function setup() {
 	createCanvas(800, 800);
-	//use windowsWidth and windowsHeight variable to get the innerwindows size for running fullscreen program.
+	//Use windowsWidth and windowsHeight variable to get the innerwindows size for running fullscreen program.
 	
-	diameter = 100;
-	ampY = height/4;
+	diameter = 100; //diameter of the ellipse
+	ampY = height/4; //max amplitude
 }
 
 function draw() {
 	background(127);
 
-	var fr = round(frameRate());
-	var fc = frameCount;
-	var radFrameCount = radians(fc);
-	var incY = sin(radFrameCount) * ampY;
+	var fr = round(frameRate()); //Get the frame rate as the round number (integer)
+	var fc = frameCount; //Get the total frame count 
+	var radFrameCount = radians(fc); //Convert frame count into radians
+	var incY = sin(radFrameCount) * ampY; //Compute a increment for Y based on sinus of radians framecount multiply by a max height. This computation allow us to create an easing effect
 
-	ellipseMode(CENTER);
-	fill(20);
-	noStroke();
-	ellipse(width/2, height/2 + incY, diameter, diameter);
+	ellipseMode(CENTER); //Draw ellipse from center
+	fill(20); //Fill the drawing with a grey value Red : 20, Green : 20, Blue : 20;
+	noStroke(); //Do not draw stroke
+	ellipse(width/2, height/2 + incY, diameter, diameter); //draw ellipse athe center of the scene
 
-	text("frameRate : "+fr, 20, 20);
-	text("FrameCount : "+fc, 20, 40);
+	text("frameRate : "+fr, 20, 20);//draw actual frame rate
+	text("FrameCount : "+fc, 20, 40);//draw actual frame count
 }
