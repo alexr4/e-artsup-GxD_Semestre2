7ace5012118aab3bea5445a3efa35bdebd649017
Alexandre Rivaux <alex@bonjour-lab.com>
2017-02-26 19:41:07 +0100

    20160226 Pixel Manipulation

---
+++ cours/20170226_PixelManipulation/sketch.js
@@ -0,0 +1,13 @@
+var img;
+
+function preload(){
+	img = loadImage("starryNight.jpg");
+}
+
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+	image(img, 0, 0);
+}
+
+function draw(){
+}
